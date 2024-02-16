843ac3a23b20e72aaf080d4a0fb63c6c99195767
LyaminEgor <159790958+LyaminEgor@users.noreply.github.com>
2024-02-16 16:36:09 +0700

    Update 14.py

--- 14.py
+++ 14.py
@@ -1,4 +1,4 @@
 chislo=int (input("Напишите исходное число:"))
 resultat = ((chislo + 2) * 3 - 6) / 3 - 4
 print(resultat)
-#Можем также упростить выражение таким образом: resultat = chislo / 3 - 4
+# Можем также упростить выражение таким образом: resultat = chislo / 3 - 4
